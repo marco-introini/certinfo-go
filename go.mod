fbf0c65e8c234df462e346265d7597882cc3331c
Marco "Mint" Introini <mint.dev@pm.me>
2024-02-01 15:00:45 +0100

    First Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module certinfo-go
+
+go 1.21
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	args := os.Args
+	if len(args) != 2 {
+		fmt.Println("You must provide exactly one argument")
+		os.Exit(1)
+	}
+
+	if _, err := os.Stat(args[1]); err != nil {
+		if os.IsNotExist(err) {
+			fmt.Println("File does not exist:", args[1])
+			os.Exit(1)
+		} else {
+			fmt.Println("Error checking file:", err)
+			os.Exit(1)
+		}
+	}
+
+	fmt.Println("File exists:", args[1])
+
+	file := args[1]
+	f, err := os.Open(file)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	pemData, err := io.ReadAll(f)
+	if err != nil {
+		fmt.Println("Error reading data from file:", err)
+		os.Exit(1)
+	}
+
+	// Check if the file is a valid PEM certificate.
+	block, rest := pem.Decode(pemData)
+	if len(rest) != 0 || block == nil {
+		fmt.Println("File is not a valid PEM certificate")
+		os.Exit(1)
+	}
+
+	cert, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		fmt.Println("File is not a valid PEM certificate:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Certificate Details:", cert)
+
+}
